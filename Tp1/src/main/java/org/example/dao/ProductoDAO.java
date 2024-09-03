5fadc4192276b75f077ac57ed4302cdb288abd6c
Matias <matiasschmitz@gmail.com>
2024-09-03 16:44:44 -0300

    cambios primer juntada

---
+++ Tp1/src/main/java/org/example/dao/ProductoDAO.java
@@ -0,0 +1,62 @@
+package org.example.dao;
+
+import org.example.entities.Cliente;
+import org.example.entities.Producto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoDAO {
+    private Connection conn;
+
+    public ProductoDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public void insertProducto(Producto producto) {
+        String query = "INSERT INTO producto (id, nombre, valor) VALUES (?, ?, ?)";
+        PreparedStatement ps = null;
+
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, producto.getId());
+            ps.setString(2, producto.getNombre());
+            ps.setFloat(3, producto.getValor());
+            ps.executeUpdate();
+            System.out.println("Producto insertada exitosamente.");
+        } catch (
+                SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                conn.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<Producto> selectAll() {
+        String query = "SELECT * FROM producto";
+        PreparedStatement ps;
+        List<Producto> resultado = new ArrayList<>();
+        try {
+            ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                resultado.add(new Producto(rs.getInt(1),rs.getString(2),rs.getFloat(3)));
+            }
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return resultado;
+    }
+}
---
+++ Tp1/src/main/java/org/example/factory/MySQLDAOFactory.java
@@ -0,0 +1,68 @@
+package org.example.factory;
+
+
+import org.example.dao.ClienteDAO;
+import org.example.dao.FacturaDAO;
+import org.example.dao.ProductoDAO;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLDAOFactory extends AbstractFactory{
+    private static MySQLDAOFactory instance = null;
+
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String uri = "jdbc:mysql://localhost:3306/db-tpe";
+    public static Connection conn;
+
+    private MySQLDAOFactory() {
+    }
+
+    public static synchronized MySQLDAOFactory getInstance() {
+        if (instance == null) {
+            instance = new MySQLDAOFactory();
+        }
+        return instance;
+    }
+
+    public static Connection createConnection() {
+        if (conn != null) {
+            return conn;
+        }
+        String driver = DRIVER;
+        try {
+            Class.forName(driver).getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+                 | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            conn = DriverManager.getConnection(uri, "root", "password");
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    @Override
+    public ClienteDAO getClienteDAO() {
+        return new ClienteDAO(createConnection());
+    }
+
+    @Override
+    public FacturaDAO getFacturaDAO() {
+        return new FacturaDAO(createConnection());
+    }
+
+    @Override
+    public ProductoDAO getProductoDAO() {
+        return new ProductoDAO(createConnection());
+    }
+
+
+}
