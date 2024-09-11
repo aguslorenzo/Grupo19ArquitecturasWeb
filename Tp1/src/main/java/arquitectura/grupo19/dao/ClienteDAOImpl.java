15d35c561b26cb9d1b8f0d06ab8fb86c2585d358
Aguus Lorenzo <aguslorenzo21@gmail.com>
2024-09-11 10:43:42 -0300

    fix de la consulta del punto 4 que retornaba clientes repetidos

--- Tp1/src/main/java/arquitectura/grupo19/dao/ClienteDAOImpl.java
+++ Tp1/src/main/java/arquitectura/grupo19/dao/ClienteDAOImpl.java
@@ -78,7 +78,8 @@ public class ClienteDAOImpl implements ClienteDAO{
 
     public List<Cliente> obtenerFacturacionClientes() {
         List<Cliente> clientes = new ArrayList<>();
-        String query = "SELECT c.idCliente, c.nombre, c.email, f.idFactura, f.total_factura " +
+        //TODO dejo esto comentado para que vean lo que cambie en la consulta para que funcione
+        /*String query = "SELECT c.idCliente, c.nombre, c.email, f.idFactura, f.total_factura " +
                         "FROM clientes c " +
                         "JOIN ( " +
                         "    SELECT f.idFactura, f.idCliente, SUM(fp.cantidad * p.valor) AS total_factura " +
@@ -88,7 +89,18 @@ public class ClienteDAOImpl implements ClienteDAO{
                         "    GROUP BY f.idFactura, f.idCliente " +
                         ") AS f " +
                         "ON c.idCliente = f.idCliente " +
-                        "ORDER BY f.total_factura DESC";
+                        "ORDER BY f.total_factura DESC";*/
+        String query = "SELECT c.idCliente, c.nombre, c.email, SUM(f.total_factura) AS total_facturado" +
+        "FROM clientes c " +
+        "JOIN ( " +
+                " SELECT f.idCliente, SUM(fp.cantidad * p.valor) AS total_factura " +
+                " FROM facturas f " +
+                " JOIN facturas_productos fp ON f.idFactura = fp.idFactura " +
+                " JOIN productos p ON fp.idProducto = p.idProducto " +
+                " GROUP BY f.idCliente " +
+        " ) AS f ON c.idCliente = f.idCliente " +
+        " GROUP BY c.idCliente, c.nombre, c.email " +
+        " ORDER BY total_facturado DESC";
 
         try (PreparedStatement stmt = conn.prepareStatement(query);
              ResultSet rs = stmt.executeQuery()) {
