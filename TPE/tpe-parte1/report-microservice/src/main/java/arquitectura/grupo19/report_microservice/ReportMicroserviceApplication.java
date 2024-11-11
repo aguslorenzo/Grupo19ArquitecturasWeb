479604f6f6a6689d5b86fcfffb33af09c25ab8de
Matias <matiasschmitz@gmail.com>
2024-11-11 18:42:03 -0300

    correcciones por falta de db en este micro (por ahora), conexion a eureka solucionada

--- TPE/tpe-parte1/report-microservice/src/main/java/arquitectura/grupo19/report_microservice/ReportMicroserviceApplication.java
+++ TPE/tpe-parte1/report-microservice/src/main/java/arquitectura/grupo19/report_microservice/ReportMicroserviceApplication.java
@@ -2,8 +2,10 @@ package arquitectura.grupo19.report_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReportMicroserviceApplication {
 
 	public static void main(String[] args) {
