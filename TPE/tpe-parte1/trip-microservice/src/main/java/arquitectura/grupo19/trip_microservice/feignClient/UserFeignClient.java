2e86bc4db71b0126ac5cd57fc943e26ba6cf12b2
Matias <matiasschmitz@gmail.com>
2024-11-11 18:15:01 -0300

    arreglados detalles para que feign funcione, creado de asociacion con eureka service

--- TPE/tpe-parte1/trip-microservice/src/main/java/arquitectura/grupo19/trip_microservice/feignClient/UserFeignClient.java
+++ TPE/tpe-parte1/trip-microservice/src/main/java/arquitectura/grupo19/trip_microservice/feignClient/UserFeignClient.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "user-microservice")
 public interface UserFeignClient {
@@ -12,13 +13,13 @@ public interface UserFeignClient {
     @GetMapping("api/users/id/{id}")
     User getUserById(@PathVariable long id);
 
-    @GetMapping("api/users/id/{id}/minbalance/{cost}")
-    boolean hasSufficientBalance(long id, double cost);
+    @GetMapping("api/users/id/{id}/minbalance/{cost}") //TODO repensar si esto es un path variable o query param
+    boolean hasSufficientBalance(@PathVariable long id,@PathVariable double cost);
 
-    @PostMapping("api/users/id/{id}/deduct/{cost}")
-    void deductBalance(long id, double cost);
+    @PostMapping("api/users/id/{id}/deduct/{cost}") //TODO repensar si esto es un path variable o query param
+    void deductBalance(@PathVariable long id,@PathVariable double cost);
 
     @PostMapping("api/users/id/{id}/notify/{message}")
-    void notifyUser(long id, String message);
+    void notifyUser(@PathVariable long id, @RequestBody String message);
 
 }
