ededbcf7230a2ea4ad33bae8f930d03676992e00
matt <matias.schmitz@gmail.com>
2024-11-17 10:00:45 -0300

    agregado trip a api-gateway + cambio en userfeignclient de trip + agregada variable nombre de tabla en la entidad trip

--- TPE/tpe-parte2/trip-microservice/src/main/java/arquitectura/grupo19/trip_microservice/entities/Trip.java
+++ TPE/tpe-parte2/trip-microservice/src/main/java/arquitectura/grupo19/trip_microservice/entities/Trip.java
@@ -1,9 +1,6 @@
 package arquitectura.grupo19.trip_microservice.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.Locale;
 
 @Entity
+@Table(name = "trip")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- TPE/tpe-parte2/trip-microservice/src/main/java/arquitectura/grupo19/trip_microservice/feignClient/UserFeignClient.java
+++ TPE/tpe-parte2/trip-microservice/src/main/java/arquitectura/grupo19/trip_microservice/feignClient/UserFeignClient.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "user-microservice", url="http://localhost:8084/users")
+@FeignClient("USER-MICROSERVICE")
 public interface UserFeignClient {
 
-    @GetMapping("/{id}")
+    @GetMapping("users/{id}")
     User getUserById(@PathVariable long id);
 
-    @GetMapping("/{id}/minbalance/{cost}") //TODO repensar si esto es un path variable o query param
+    @GetMapping("users/{id}/minbalance/{cost}") //TODO repensar si esto es un path variable o query param
     boolean hasSufficientBalance(@PathVariable long id,@PathVariable double cost);
 
-    @PostMapping("/{id}/deduct/{cost}") //TODO repensar si esto es un path variable o query param
+    @PostMapping("users/{id}/deduct/{cost}") //TODO repensar si esto es un path variable o query param
     void deductBalance(@PathVariable long id,@PathVariable double cost);
 
-    @PostMapping("/{id}/notify/")
+    @PostMapping("users/{id}/notify/")
     void notifyUser(@PathVariable long id, @RequestBody String message);
 
 }
